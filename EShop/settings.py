e63ad598eb16d2740eb73362d884d38d3d935338
Abhimanyu <abhimanyu250101@gmail.com>
2025-11-24 00:09:27 +0530

    preparing for deployment

--- EShop/settings.py
+++ EShop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from .env import *
 from django.conf import settings
@@ -86,10 +87,7 @@ WSGI_APPLICATION = 'EShop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
